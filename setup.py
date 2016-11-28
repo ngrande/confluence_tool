920fcee45287033484c1ced1391a99945b327e73
ngrande <devlup@outlook.com>
2016-11-28 18:20:43 +0100

    made the setup a bit more 'modern' and added license

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('LICENSE', 'r') as f:
+    license = f.read()
 
 setup(name='ticketmanager',
-      version='0.1',
+      version='0.0.1',
       description='Provides an interface to query information from a \
       confluence website (especially to get the ticketmanager timetable data)',
-      author='ngrande',
-      author_email='devlup@outlook.com'
-      packages=['ticketmanager'],
+      long_description=readme,
+      author='Niclas Grande',
+      license=license,
+      author_email='devlup@outlook.com',
+      url='https://github.com/ngrande/confluence_tool',
+      packages=find_packages(exclude=('tests', 'docs')),
       install_requires=['bs4', 'aiohttp'],
       scripts=['who_is_ticketmanager'])
