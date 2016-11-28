261b976a94621ed84d4e2d0d27535f762d537fbf
ngrande <devlup@outlook.com>
2016-11-28 18:07:12 +0100

    Updated package description

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup
 
 setup(name='ticketmanager',
       version='0.1',
-      description='Shows the dates when you are ticketmanager and tells you \
-                   if this day is today',
+      description='Provides an interface to query information from a \
+      confluence website (especially to get the ticketmanager timetable data)',
       author='ngrande',
+      author_email='devlup@outlook.com'
       packages=['ticketmanager'],
       install_requires=['bs4', 'aiohttp'],
       scripts=['who_is_ticketmanager'])
