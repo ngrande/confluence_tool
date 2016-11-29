dc47515f4003910d28440adf0b3fa6f3c3dd93d0
ngrande <devlup@outlook.com>
2016-11-29 15:11:00 +0100

    optimized output and loop

--- ticketmanager/__init__.py
+++ ticketmanager/__init__.py
@@ -69,7 +69,7 @@ def read_config():
     return (proxy_cfg, url_cfg, username_cfg, password_cfg)
 
 
-async def parse_json_response(json_res):
+def parse_json_response(json_res):
     json_data = json.loads(json_res)
     html_data = json_data['body']['view']['value']
     return html_data
@@ -145,14 +145,34 @@ async def download_ticketmanager_site(session):
             return await response.text()
 
 
-def print_beautiful(ticketmanager):
+def print_upcoming_table(ticketmanager):
     today = datetime.date.today()
     upcoming = [day for day in ticketmanager.tm_days if day > today]
     print('Upcoming:')
+    print("|     Date     |       Days       |")
     for day in upcoming:
-        print('\t| %s %02d %02d | X |' % (day.year, day.month, day.day))
+        days_left = day - today
+        print('+--------------+------------------+')
+        print('| %s %02d %02d   |        %02d        |' % (day.year,
+                                                           day.month,
+                                                           day.day,
+                                                           days_left.days))
 
-    print()
+    print("\n")
+
+
+def print_time_till_next(ticketmanager):
+    today = datetime.date.today()
+    upcoming = [day for day in ticketmanager.tm_days if day > today]
+    if not upcoming or not len(upcoming) > 0:
+        print("There is no next time for you...")
+        print("This could be useful for you:")
+        print("https://stackoverflow.com/jobs")
+    else:
+        next = upcoming[0] - today
+        print("Next duty in %d day(s)" % next.days)
+
+    print("\n")
 
 
 def print_status(ticketmanager):
@@ -163,29 +183,30 @@ def print_status(ticketmanager):
         print('#                                 #')
         print("###################################")
     else:
-        print("+----------------------------------+")
-        print("| Today is not your day...         |")
-        print("+----------------------------------+")
+        print("It's not your duty today...")
+
+    print("\n")
 
 
-async def check(loop, name):
-    async with aiohttp.ClientSession(loop=loop, ) as session:
+async def request_ticketmanager(loop, name):
+    async with aiohttp.ClientSession(loop=loop) as session:
 
         name = str(name).lower()
 
         json_response = await download_ticketmanager_site(session)
-        html_data = await parse_json_response(json_response)
+    html_data = parse_json_response(json_response)
 
-        tables = find_tables(html_data)
-        ticketmanagers_dict = {}
+    tables = find_tables(html_data)
+    ticketmanagers_dict = {}
 
-        for table in tables:
-            for tm in extract_ticketmanagers_from_table(table):
-                if tm.name in ticketmanagers_dict.keys():
-                    tm.merge(ticketmanagers_dict[tm.name])
-                ticketmanagers_dict[tm.name] = tm
+    for table in tables:
+        for tm in extract_ticketmanagers_from_table(table):
+            if tm.name in ticketmanagers_dict.keys():
+                tm.merge(ticketmanagers_dict[tm.name])
+            ticketmanagers_dict[tm.name] = tm
 
-        if name in ticketmanagers_dict.keys():
-            tm = ticketmanagers_dict[name]
-            print_beautiful(tm)
-            print_status(tm)
+    if name in ticketmanagers_dict.keys():
+        tm = ticketmanagers_dict[name]
+        return tm
+    else:
+        return None
